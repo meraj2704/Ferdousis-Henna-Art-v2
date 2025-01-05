973fe9be7f1aa40f1013c48f37f0159d2ee9bc97
meraj2704 <mh1669101@gmail.com>
2025-01-06 01:02:53 +0600

    update code

--- src/api/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const url = "https://ferdousis-henna-art-backend.vercel.app/api/";
-
-const axiosInstance = axios.create({
-  baseURL: url,
-});
-
-export default axiosInstance;
