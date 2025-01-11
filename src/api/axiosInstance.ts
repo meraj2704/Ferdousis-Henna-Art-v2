037d7172309b0187f747668ae58c966c74c2ae92
meraj2704 <mh1669101@gmail.com>
2025-01-12 01:07:38 +0600

    update code

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const url = "https://api.ferdousishenna.cfd/api/";
-export const url = "http://localhost:4040/api/";
+export const url = "https://api.ferdousishenna.cfd/api/";
+// export const url = "http://localhost:4040/api/";
 
 const axiosInstance = axios.create({
   baseURL: url,
